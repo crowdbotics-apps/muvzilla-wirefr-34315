0d1e1ac2b27113cd600528c2d486bc5d5a8cf09a
Crowdbotics <team@crowdbotics.com>
2022-03-19 21:57:45 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native";
+
+export const globalOptions = {
+  name: "muvzilla_wirefr_34315",
+  url: "https://muvzilla_wirefr_34315.botics.co",
+  api: "https://muvzilla_wirefr_34315.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    "copy": "Routes available!"
+  }
+}
